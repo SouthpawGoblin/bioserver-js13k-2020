1e0ed0f00ad6a7bd06fc0e83c97074e032d92f09
Yinzhe_Qi <goblin-qyz@163.com>
2020-08-15 22:30:43 +0800

    feat: basic setup

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.ts'
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
+};
